3b6352ed1b3b41e4d0fa6c7cec78a05b52c0b9d9
Yukitoshi <yukitoshi.imaizumizhou@gmail.com>
2024-10-12 23:06:01 +1100

    first query done

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,4 +1,9 @@
 -- 1. List the employee number, last name, first name, sex, and salary of each employee.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM Employees AS e
+INNER JOIN Salaries AS s
+ON e.emp_no = s.emp_no
+
 
 -- 2. List the first name, last name, and hire date for the employees who were hired in 1986.
 
