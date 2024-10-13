4af9d4b3a22499c93bbff7b72f833b70feadb467
Yukitoshi <yukitoshi.imaizumizhou@gmail.com>
2024-10-13 13:27:05 +1100

    eighth query complete

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -77,3 +77,7 @@ ORDER BY emp_no;
 
 
 -- 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT last_name, COUNT(last_name) AS Frequency
+FROM Employees
+GROUP BY last_name
+ORDER BY Frequency DESC;
